a78737b57c2359e898031d63ea47c705f9ae4c64
akki87 <akkiyogi231@gmail.com>
2023-06-25 23:29:45 +0530

    User  interfaces is in progress

---
+++ PodCastLive/AgentLogin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace PodCastLive
+{
+    public class AgentLogin : Page
+    {
+
+    }
+}
---
+++ PodCastLive/UserLogin.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace PodCastLive
+{
+    public class UserLogin : Page
+    {
+    }
+}
